4fe1f383dc43d1678ffb5d054959f75245cc7ac3
ARADITY <xpheno.aditya@mercedes-benz.com>
2025-05-25 00:40:57 +0530

    added app banner

--- app/src/main/java/com/arun/aklauncher/AppGridBottomSheet.kt
+++ app/src/main/java/com/arun/aklauncher/AppGridBottomSheet.kt
@@ -5,14 +5,19 @@ import android.content.pm.ApplicationInfo
 import android.content.pm.LauncherApps
 import android.os.UserManager
 import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.MailOutline
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -26,7 +31,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -35,6 +42,7 @@ fun AppGridBottomSheet() {
     val context = LocalContext.current
     val sheetState = rememberModalBottomSheetState()
     var showSheet by remember { mutableStateOf(false) }
+    val backgroundPainter = painterResource(id = R.drawable.ak_launcher_banner)
 
     val pm = context.packageManager
 
@@ -58,10 +66,32 @@ fun AppGridBottomSheet() {
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(onClick = { showSheet = true }) {
-                Icon(Icons.Default.MailOutline, contentDescription = "Show Apps")
+                Icon(Icons.Filled.AddCircle, contentDescription = "Show Apps")
             }
         }
     ) { innerPadding ->
+        // Background image
+        Image(
+            painter = backgroundPainter,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Box (modifier = Modifier.fillMaxSize(),){
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(3),
+                modifier = Modifier
+                    .fillMaxHeight(0.7f)
+                    .padding(8.dp),
+                contentPadding = PaddingValues(8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                items(apps) { resolveInfo ->
+                    AppItem(resolveInfo = resolveInfo, pm = pm)
+                }
+            }
+        }
         if (showSheet) {
             ModalBottomSheet(
                 onDismissRequest = { showSheet = false },
